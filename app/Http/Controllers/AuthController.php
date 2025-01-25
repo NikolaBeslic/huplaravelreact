5b9c9bf9c71afaddb41c2163a5b96f3749f65f70
beslicn <nikola_beslic@yahoo.com>
2025-01-25 15:06:52 +0100

    git registracija validacija

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Korisnik;
-
-
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -14,26 +13,43 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        // $fields = $request->validate([
-        //     "korisnickoIme" => 'required|string',
-        //     "email" => 'required|string|unique:korisnik,email',
-        //     "password" => 'required|string|confirmed'
-        // ]);
+        $validator = Validator::make($request->all(), [
+            "korisnickoIme" => 'required|string|unique:korisnik,korisnicko_ime',
+            "email" => 'required|string|email|unique:korisnik,email',
+            "password" => 'required|string|confirmed'
+        ], [
+            'korisnickoIme.unique' => 'Ovo korisnicko ime je zauzeto',
+            'email.unique' => 'Vec postoji nalog sa ovom email adresom',
+            'email.email' => 'Email adresa nije ispravna',
+            'password.confirmed' => 'Lozinke se ne poklapaju'
+        ]);
 
-        $user = Korisnik::create([
+        if ($validator->fails()) {
+            return response()->json([
+                "message" => 'validation_error',
+                "errors" => $validator->errors(),
+            ], 422);
+        }
+
+
+
+        if (Korisnik::create([
             'korisnicko_ime' => $request['korisnickoIme'],
             'email' => $request['email'],
             'password' => bcrypt($request['password']),
-        ]);
+        ])) {
+            return response()->json();
+        }
+        return response()->json("Nepoznata greska", 500);
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        // $token = $user->createToken('myapptoken')->plainTextToken;
 
-        $response = [
-            'user' => $user,
-            'token' => $token,
-        ];
+        // $response = [
+        //     'user' => $user,
+        //     'token' => $token,
+        // ];
 
-        return response($response, 201);
+
     }
 
     public function login(Request $request)
