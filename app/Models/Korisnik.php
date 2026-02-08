f31c1dad2518943ac4b9058eec13cf9bce5c3bb8
beslicn <nikola_beslic@yahoo.com>
2026-02-08 13:14:00 +0100

    git korisnicki profil layout

--- app/Models/Korisnik.php
+++ app/Models/Korisnik.php
@@ -13,30 +13,61 @@ class Korisnik extends Authenticatable
     protected $table = 'korisnik';
 
     protected $fillable = [
-        'korisnicko_ime', 'email', 'password', 'provider', 'provider_id'
+        'korisnicko_ime',
+        'email',
+        'password',
+        'provider',
+        'provider_id'
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     public function listaZelja()
     {
-        return $this->belongsToMany(Predstava::class, 'listazelja', 'korisnikid', 'predstavaid')->where('statuszeljeid', 1);
+        return $this->belongsToMany(
+            Predstava::class,
+            'listazelja',
+            'korisnikid',
+            'predstavaid'
+        )
+            ->select('predstava.predstavaid', 'naziv_predstave', 'predstava_slug', 'plakat')
+            ->wherePivot('statuszeljeid', 1)
+            ->with([
+                'pozorista:pozoristeid,naziv_pozorista',
+                'narednoIgranje'
+            ]);
     }
 
     public function listaOdgledanih()
     {
-        return $this->belongsToMany(Predstava::class, 'listazelja', 'korisnikid', 'predstavaid')->where('statuszeljeid', 2);
+        return $this->belongsToMany(
+            Predstava::class,
+            'listazelja',
+            'korisnikid',
+            'predstavaid'
+        )
+            ->select('predstava.predstavaid', 'naziv_predstave', 'predstava_slug', 'plakat')
+            ->wherePivot('statuszeljeid', 2)
+            ->with('ocenaKorisnika')
+            ->with([
+                'pozorista:pozoristeid,naziv_pozorista,pozoriste_slug'
+            ]);
     }
 
     public function omiljenaPozorista()
     {
-        return $this->belongsToMany(Pozoriste::class, 'omiljena_pozorista', 'korisnikid', 'pozoristeid');
+        return $this->belongsToMany(Pozoriste::class, 'omiljena_pozorista', 'korisnikid', 'pozoristeid')
+            ->select('pozoriste.pozoristeid', 'naziv_pozorista', 'pozoriste_slug', 'url_logo', 'gradid')
+            ->with('grad');
     }
 
     public function komentari()
     {
-        return $this->hasMany(Komentar::class, 'korisnikid');
+        return $this->hasMany(Komentar::class, 'korisnikid')
+            ->select('komentarid', 'korisnikid', 'predstavaid', 'tekst_komentara', 'created_at')
+            ->with(['predstava:predstavaid,naziv_predstave,predstava_slug']);
     }
 }
